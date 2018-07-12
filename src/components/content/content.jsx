0f51582e22ad5a17b8d7150f0ae2a1a50cfeedf7
klupzor <lopezgand@gmail.com>
2018-07-12 11:02:30 -0500

    adding pages on content

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -11,16 +11,57 @@ function Content (props){
             </div>
         )
     }
-
-
+//---------------------------------------
+    function cartaContent(){
+        return(
+            <div className="content">
+                <div className="carta" >Aqui va el Menu</div>
+            </div>
+        )
+    }
+//-----------------------------------------------
+    function aboutUsContent(){
+        return(
+            <div className="content">
+                <div className="about-us" >Quienes somos</div>
+            </div>
+        )
+    }
+//------------------------------------------------
+    function contactUsContent(){
+        return(
+            <div className="content">
+                <div className="contact-us" >Cotactenos</div>
+            </div>
+        )
+    }
+//--------------------------------------------------
+    function profileContent(){
+        return(
+            <div className="content">
+                <div className="profile" >Perfil</div>
+            </div>
+        )
+    }
+//----------------------------------------------------
     switch (props.filter) {
         case NavigationFilters.SHOW_HOME_CONTENT:
             return homeContent()
+        
+        case NavigationFilters.SHOW_CARTA:
+            return cartaContent()
+
+        case NavigationFilters.SHOW_ABOUT_US:
+            return aboutUsContent()
+
+        case NavigationFilters.SHOW_CONTACT_US:
+            return contactUsContent()
+
+        case NavigationFilters.SHOW_PROFILE:
+            return profileContent()
     
         default:
-            return(
-                <div>hola bb</div>
-            )
+            return homeContent()
     }
     
 }
