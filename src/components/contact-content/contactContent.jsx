7f0ddb04d2da14d938041f23e8a15ccce580d7dd
klupzor <lopezgand@gmail.com>
2018-07-19 14:46:52 -0500

    Configurando  googlemaps en el proyecto

--- src/components/contact-content/contactContent.jsx
+++ src/components/contact-content/contactContent.jsx
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
+import scriptLoader from 'react-async-script-loader'
 import {connect} from 'react-redux'
 import { setDataCategory, setDataContact, modifyContact } from '../../actions';
+
+@scriptLoader(['https://maps.googleapis.com/maps/api/js?key=AIzaSyD0nBkgLXCVDDnJ3Ba7sQ62TI5yxS8sfzA'])
 
 class ContactContent extends Component{
     constructor(props){
@@ -17,8 +20,24 @@ class ContactContent extends Component{
         event.preventDefault()
         this.props.dispatch(modifyContact(this.props, this.props.llave))
     }
+
+    componentWillReceiveProps({isScriptLoadSucceed}){
+        if (isScriptLoadSucceed) {
+            var markers = [];
+
+            var map = new window.google.maps.Map(document.getElementById('map'), {
+                zoom: 12,
+                center: {lat: 37.7749300, lng: -122.4194200}
+            });
+        }
+        else{
+            alert("script not loaded")
+        }
+    }
     render(){
+              
         return(
+            
             <div className="content">
                 <div className="contact-us" >
                     <form  onSubmit={this.handleSubmit} >
@@ -34,9 +53,13 @@ class ContactContent extends Component{
                         <label >Dirección del negocio 
                             <input type="text" name="address" value={this.props.address} onChange={this.handleChange} />
                         </label>
+                        <button>Visualizar</button>
                         <input type="submit" value="Actualizar" />
                     </form>
                 </div>
+                <div id="map" style={{height: "600px"}}></div>
+                
+
             </div>
         )
 
